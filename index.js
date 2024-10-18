78f9aae0f63f244bb6e48574a4bb0d6d4bb3161a
fahad09fahim <anik.fahim07@gmail.com>
2024-10-18 20:59:15 +0600

    spinning and show all completed

--- index.js
+++ index.js
@@ -3,19 +3,48 @@
 // https://openapi.programming-hero.com/api/phone/${id}
 
 //get phone data
-const phoneData = async (searchText) => {
-  const response = await fetch(
-    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-  );
-  const phoneData = await response.json();
-  const phones = phoneData.data;
-  displayPhones(phones);
+const phoneData = async (searchText, isShowAll) => {
+  try {
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    );
+    if (!response.ok) {
+      throw new Error("Phone not found");
+    }
+
+    const phoneData = await response.json();
+    const phones = phoneData.data;
+
+    if (phones.length === 0) {
+      alert("Phone not found");
+      location.reload();
+      return;
+    }
+
+    displayPhones(phones);
+  } catch (error) {
+    // Handle network or other errors
+    console.error(error);
+    alert(error.message || "An error occurred");
+  }
 };
 
-const displayPhones = (phones) => {
+// handle display phones
+const displayPhones = (phones, isShowAll) => {
   const phoneContainer = document.getElementById("phone-container");
 
   phoneContainer.textContent = " ";
+
+  const showAll = document.getElementById("show-all-container");
+  if (phones.length > 15 && !isShowAll) {
+    showAll.classList.remove("hidden");
+  } else {
+    showAll.classList.add("hidden");
+  }
+  if (!isShowAll) {
+    phones = phones.slice(0, 15);
+  }
+
   phones.forEach((phone) => {
     // console.log(phone);
     const phoneCard = document.createElement("div");
@@ -39,13 +68,30 @@ const displayPhones = (phones) => {
     `;
     phoneContainer.appendChild(phoneCard);
   });
+  toggleSpinner(false);
 };
 
 // handle search function
 
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   const searchBox = document.getElementById("search-box");
   const searchText = searchBox.value;
   // console.log(searchText);
-  phoneData(searchText);
+  toggleSpinner(true);
+  phoneData(searchText, isShowAll);
+};
+
+// handle loading function
+const toggleSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
+
+// handle show all button click
+const handleShowAll = () => {
+  handleSearch(true);
 };
